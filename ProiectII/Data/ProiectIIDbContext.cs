24247080c13c0e50a09a7b0f6a90729d411934d0
Radu <Crisan.So.Radu@student.utcluj.ro>
2024-04-13 14:05:42 +0300

    ProiectII

---
+++ ProiectII/Data/ProiectIIDbContext.cs
@@ -0,0 +1,11 @@
+﻿using ProiectII.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProiectII.Data
+{
+    public class ProiectIIDbContext : DbContext
+    {
+        public ProiectIIDbContext(DbContextOptions options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+    }
+}
